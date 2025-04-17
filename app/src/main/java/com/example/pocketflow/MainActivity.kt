eb299329d7a8598ca30ba075dc2951ba5490bfdb
Nitban <tiggertok@gmail.com>
2025-04-17 15:01:54 -0600

    nada importante

--- app/src/main/java/com/example/pocketflow/MainActivity.kt
+++ app/src/main/java/com/example/pocketflow/MainActivity.kt
@@ -24,7 +24,7 @@ fun PocketFlowApp() {
 
     NavHost(
         navController = navController,
-        startDestination = "inicio" // La primera pantalla que se muestra
+        startDestination = "perfil" // La primera pantalla que se muestra
     ) {
         composable("perfil") { PerfilScreen(navController) }
         composable("categorias") { CategoriasScreen() }
