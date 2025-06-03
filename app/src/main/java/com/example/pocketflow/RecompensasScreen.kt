a352c74dd6d3de7b159903aa3ba18ec871b94f4c
PilarMora <a22310352@ceti.mx>
2025-06-03 09:56:59 -0600

    Recompensa lista

--- app/src/main/java/com/example/pocketflow/RecompensasScreen.kt
+++ app/src/main/java/com/example/pocketflow/RecompensasScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.clipPath
 import com.example.pocketflow.ui.theme.TopBar
 
+
 @Composable
 fun RecompensasScreen(navController: NavHostController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
